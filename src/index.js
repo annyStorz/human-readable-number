a772ea8e565dcff08bc115acdefab0455a415720
Anna Storozhenko <AnnyStorozhenko@gmail.com>
2021-08-02 13:07:18 +0400

    fix: fix zero reading

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 module.exports = function toReadable (number) {
-    let first = ["zero " ,"one ", "two ", "three ", "four ", "five ", "six ", "seven", "eight ", "nine "];
+    let first = ["zero " ,"one ", "two ", "three ", "four ", "five ", "six ", "seven ", "eight ", "nine "];
     let secondFrom1 = ["ten ", "eleven ", "twelve ", "thirteen ", "fourteen ", "fifteen ", "sixteen ", "seventeen ", "eighteen ", "nineteen "];
     let secondOthers = ["twenty ", "thirty ", "forty ", "fifty ", "sixty ", "seventy ", "eighty ", "ninety "];
     let words = "";
-    number = number.replace(/(\d)/g, '$1 ');
-    let result = number.split(' ');
+    let num = String(number);
+    let number2 = num.replace(/(\d)/g, '$1 ');
+    let result = number2.split(' ');
     for (let i = 0; i <= result.length; ++i){
     	if (result[i] != 0){
-    		if (result.length == 4 && result[2] == 0 && result[1] == 0 && i == 0){
+    		if (result.length == 4 && result.length - i == 4 && i == 0){
             	words += first[result[i]] + "hundred ";
         	}
-            else if (result.length == 4 && result.length - i == 4){
-            	words += first[result[i]] + "hundred and ";
-            }
     	    if (result[i] != 1 && result.length - i == 3){
         	    words += secondOthers[result[i]-2];
             }
@@ -25,5 +23,10 @@ module.exports = function toReadable (number) {
                 break;
             }
         }
+        else if (words == "") {
+        	words = "zero ";
+        }
     }
+    let res = words.slice(0, -1);
+    return res;
 }
